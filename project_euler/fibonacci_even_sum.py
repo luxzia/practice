bfb2a9a71f9a4da6ff43148d398e172eb53591f0
luxzia <luxzia@gmail.com>
2014-09-09 00:06:34 -0700

    adding project euler files

---
+++ project_euler/fibonacci_even_sum.py
@@ -0,0 +1,20 @@
+def fibonacci_even_sum(num):
+    """
+    INPUT: an integer greater than or equal to zero
+    OUTPUT: the sum of all even Fibonacci numbers less than the ith Fibonacci number,
+            where i is equal to the input
+    """
+    even_lst = []
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    first_num = 1
+    second_num = 1
+    while second_num <= num:
+        temp_num = second_num
+        second_num = first_num + second_num
+        first_num = temp_num
+        if second_num % 2 == 0:
+           even_lst.append(second_num)
+    return sum(even_lst)
---
+++ project_euler/multiples_of_three_and_five.py
@@ -0,0 +1,3 @@
+multiple_list = [x for x in xrange(1000) if x % 5 == 0 or x % 3 == 0]
+multiples_sum = sum(multiple_list)
+
